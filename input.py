1cd1e2ba884a6b2028e9a77393b28adcf26cc9ca
herlanalansuherman31 <herlanalansuherman06@gmail.com>
2025-11-17 14:56:11 +0700

    kasir sederhana

---
+++ input.py
@@ -0,0 +1,8 @@
+item = input("apa yang kamu beli = ")
+harga = float(input("berapa harga itemnya = "))
+jumlah = int(input("berapa jumlahnya = "))
+total = harga*jumlah
+
+print("\n=======================================================")
+print (f"item = {item}, jumlahnya = {jumlah}, harga = {harga}")
+print (f"total yang anda bayar = Rp{total}")
